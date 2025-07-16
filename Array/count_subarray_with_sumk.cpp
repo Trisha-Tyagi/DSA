efd6d7d3edba469d5938cce9909d8a41adf6a71f
Trisha-Tyagi <trishatyagi613@gmail.com>
2025-07-16 17:17:23 +0530

    Count of subarray with sum K

---
+++ Array/count_subarray_with_sumk.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+#include<map>
+using namespace std;
+int main(){
+    vector<int> arr={2,3,4,0,6,-7,1,2,1};
+    int k=4;
+    map<int,int> dic;
+    int n=arr.size();
+    int sum=0;
+    int count=0;
+    for(int i=0;i<n;i++){
+        sum+=arr[i];
+        int target=sum-k;
+        if(sum==k){
+            count+dic[target];
+        }
+        if(dic.find(target)!=dic.end()){
+            count+=dic[target];
+        }
+        dic[sum]++;
+
+
+    }
+    cout<<count;
+
+}
