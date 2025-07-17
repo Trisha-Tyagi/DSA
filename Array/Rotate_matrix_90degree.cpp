052fa3d56ed123b92d7ec95e758821143721db51
Trisha-Tyagi <trishatyagi613@gmail.com>
2025-07-17 20:20:01 +0530

    Array

--- Array/Rotate_matrix_90degree.cpp
+++ Array/Rotate_matrix_90degree.cpp
@@ -4,14 +4,13 @@
 using namespace std;
 
 int main(){
-    vector<vector<int>> arr={{1,2,3,4},{6,7,8,9},{11,12,34,21}};
-    int n=arr.size();
-    int m=arr[0].size();
-    vector<vector<int>> result(m, vector<int>(n, 0));
+    // vector<vector<int>> arr={{1,2,3,4},{6,7,8,9},{11,12,34,21}};
+    // int n=arr.size();
+    // int m=arr[0].size();
 
     //////////////////////////// Brute force//////////////////////////
 
-
+    // vector<vector<int>> result(m, vector<int>(n, 0));
     // for(int i=0;i<n;i++){
     //     for(int j=0;j<m;j++){
     //         result[j][n-i-1]=arr[i][j];
@@ -23,24 +22,26 @@ int main(){
     //     }
     //     cout<<endl;
     // }
-    
+
     //////////////////////////// Optimal////////////////////////////
 
-
+    vector<vector<int>> arr={{1,2,3,4},{6,7,8,9},{11,12,34,21},{0,5,9,8}};
+    int n=arr.size();
+    int m=arr[0].size();
     for(int i=0;i<n;i++){
-        for(int j=0;j<m/2;j++){
-            if(i!=j){
-                swap(arr[i][j],arr[j][i]);
-            }
+        for(int j=0;j<i;j++){
+            
+            swap(arr[i][j],arr[j][i]);
+            
 
         }
     }
     for(int i=0;i<n;i++){
-        reverse(arr[i].begin(),arr[0].end());
+        reverse(arr[i].begin(),arr[i].end());
     }
     for(int i=0;i<m;i++){
         for(int j=0;j<n;j++){
-            cout<<result[i][j]<<" ";
+            cout<<arr[i][j]<<" ";
         }
         cout<<endl;
     }
