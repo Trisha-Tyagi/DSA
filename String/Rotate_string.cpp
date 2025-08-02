a1e5af7f5665770019417bfcafc5663b5629bd03
Trisha-Tyagi <trishatyagi613@gmail.com>
2025-08-02 12:16:56 +0530

    modified

--- String/Rotate_string.cpp
+++ String/Rotate_string.cpp
@@ -1,22 +1,32 @@
 #include<iostream>
 #include<string>
 using namespace std;
-bool rotateString(string s, string goal) {
-        int n=s.size();
-        if(s.size()!=goal.size()) return false;
-        if(s==goal) return true;
-        for(int i=n-1;i>=0;i--){
-            string a=goal.substr(i,n);
-            string b=goal.substr(0,i);
-            string result=a+b;
-            if(result==s){
-                return true;
-            }
+// TC:O(n^2)
+// SC:O(n)
+// bool rotateString(string s, string goal) {
+//         int n=s.size();
+//         if(s.size()!=goal.size()) return false;
+//         if(s==goal) return true;
+//         for(int i=n-1;i>=0;i--){
+//             string a=goal.substr(i,n);
+//             string b=goal.substr(0,i);
+//             string result=a+b;
+//             if(result==s){
+//                 return true;
+//             }
 
-        }
-        return false;
+//         }
+//         return false;
         
-    }
+//     }
+
+// TC:O(n)
+// SC:O(n)
+bool rotateString(string s, string goal) {
+    if (s.size() != goal.size()) return false;
+    string double_s = s + s;
+    return double_s.find(goal) != string::npos;
+}
 int main(){
     string s1="abcde";
     string s2="cdeab";
