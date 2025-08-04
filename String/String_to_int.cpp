99f965274bfd63decfa68e4e963be0c028d2a0d1
Trisha-Tyagi <trishatyagi613@gmail.com>
2025-08-04 08:53:03 +0530

    string to integer

---
+++ String/String_to_int.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<string>
+using namespace std;
+// TC:O(n)
+// SC:O(1)
+int str_to_int(string s){
+     int n=s.size();
+    int sign=1;
+    int i=0;
+    int ans=0;
+    while(i<n && s[i]== ' ') i++;
+    if(i==n) return ans;
+    if(s[i]=='-'){ sign=0;
+        i++;}
+    else if(s[i]=='+'){
+        sign=1;
+        i++;
+    }
+    while (i < n && isdigit(s[i])) {
+            int digit = s[i] - '0';
+
+            // Check overflow BEFORE it happens
+            if (ans > (INT64_MAX - digit) / 10) {
+                return sign == 1 ? INT64_MAX : INT64_MIN;
+            }
+
+            ans = ans * 10 + digit;
+            i++;
+        }
+    if(sign==0) return -1*ans;
+    return ans;
+    }
+int main(){
+    string s="   -567ab";
+    int no=str_to_int(s);
+    cout<<"The number extracted is: "<<no;
+    return 0;
+}
